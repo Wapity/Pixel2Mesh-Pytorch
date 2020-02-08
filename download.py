426c9f77ae0941070f537b530b871149964132af
ge98lor <alexandre.cafaro@tum.de>
2020-02-08 18:26:17 +0100

    Dowloader_bis

--- download.py
+++ download.py
@@ -6,7 +6,7 @@ urls = [
     'https://drive.google.com/open?id=1nvDDweOxTxFJ2m0p2KGrDaCi5wEeIDC7'
 ]
 
-urls = [url.replace('open', 'uc')]
+urls = [url.replace('open', 'uc') for url in urls]
 for id, url in enumerate(urls):
     output = 'training_data.7z.00{}'.format(id + 1)
     print('Donwloading {} ...'.format(output))
