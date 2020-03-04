48ca3ae975cf7ca58fb695657feb583c285564be
Wapity / Alex <onealex.c@gmail.com>
2020-03-04 11:40:35 +0100

    Update download.py

--- download.py
+++ download.py
@@ -1,9 +1,9 @@
 import gdown
 urls = [
-    'https://drive.google.com/open?id=1Qo1NGS0ByB9L9MbEQ2TdoYtw1pHqJR6u&export=download',
-    'https://drive.google.com/open?id=14OmDZt41W1KXQGk5qHuraQiXe1q7RghT&export=download',
-    'https://drive.google.com/uc?id=1ElUDDpUPZULM4MGptuxJqMtUZxcGDLvJ&export=download',
-    'https://drive.google.com/open?id=1nvDDweOxTxFJ2m0p2KGrDaCi5wEeIDC7&export=download'
+    'https://drive.google.com/uc?id=1Qo1NGS0ByB9L9MbEQ2TdoYtw1pHqJR6u',
+    'https://drive.google.com/uc?id=14OmDZt41W1KXQGk5qHuraQiXe1q7RghT',
+    'https://drive.google.com/uc?id=1ElUDDpUPZULM4MGptuxJqMtUZxcGDLvJ',
+    'https://drive.google.com/uc?id=1nvDDweOxTxFJ2m0p2KGrDaCi5wEeIDC7'
 ]
 
 urls = [url.replace('open', 'uc') for url in urls]
@@ -11,3 +11,4 @@ for id, url in enumerate(urls):
     output = 'training_data.7z.00{}'.format(id + 1)
     print('Donwloading {} ...'.format(output))
     gdown.download(url, output, quiet=False)
+
