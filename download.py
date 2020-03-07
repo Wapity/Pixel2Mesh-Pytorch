1219ec4d56e88dabcc23a5b2277383b814942736
Wapity / Alex <onealex.c@gmail.com>
2020-03-07 11:35:43 +0100

    Update download.py

--- download.py
+++ download.py
@@ -1,14 +1,12 @@
 import gdown
 urls = [
-    'https://drive.google.com/uc?id=1kjLMLd_55Ro7wDrKeFCL3mT0daQ7Aepf',
-    'https://drive.google.com/uc?id=12XYrdIqZp4_jg2AJNZtCQvMC35KrnN4F',
-    'https://drive.google.com/uc?id=10N5DUf8eacNVWK7txZt3ibbxsZ84K0B9',
-    'https://drive.google.com/uc?id=149e9mcO79DEemy3ZBHfyWOGZBybspVwq'
+    'https://drive.google.com/open?id=1dKM_6sR7SCdDQXX8dIQwtpaW3qRiA7pM',
+    'https://drive.google.com/open?id=1hXq-kxEsDA0_DzcOFS0yzIvlKxwcgoGZ'
 ]
 
 urls = [url.replace('open', 'uc') for url in urls]
 for id, url in enumerate(urls):
-    output = 'training_data.7z.00{}'.format(id + 1)
+    output = 'training_data.00{}'.format(id + 1)
     print('Donwloading {} ...'.format(output))
     gdown.download(url, output, quiet=False)
 
