d901b0b24c0430e3eed92d865fbb0e0a6f429805
Wapity / Alex <onealex.c@gmail.com>
2020-03-08 22:36:00 +0100

    Update chamfer.py

--- pytorch/p2m/chamfer.py
+++ pytorch/p2m/chamfer.py
@@ -1,14 +1,20 @@
 #from .external.chamfer3D import dist_chamfer_3D
 #nn_distance_function = chamfer3D.dist_chamfer_3D.chamfer_3DFunction
 #nn_distance_module = chamfer3D.dist_chamfer_3D.chamfer_3DDist
-
+import torch
 from .external.chamfer_python import distChamfer
+from .external.chamfer3D.dist_chamfer_3D import chamfer_3DFunction
+use_cuda = torch.cuda.is_available()
+if use_cuda:
+    dist_module = chamfer_3DFunction()
+else:
+    dist_module = distChamfer
 
 
 def nn_distance_function(a, b):
     if len(a.shape) == 3:
-        return distChamfer(a, b)
+        return dist_module(a, b)
     else:
-        dist1, dist2, idx1, idx2 = distChamfer(a.unsqueeze(0), b.unsqueeze(0))
+        dist1, dist2, idx1, idx2 = dist_module(a.unsqueeze(0), b.unsqueeze(0))
         return dist1.squeeze(0), dist2.squeeze(0), idx1.squeeze(
             0), idx2.squeeze(0)
