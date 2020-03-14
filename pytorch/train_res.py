2f6cc05bb6c9206e5ea97692c76e423be02ad099
ge98lor <alexandre.cafaro@tum.de>
2020-03-14 19:28:19 +0100

    Update train_res.py

--- pytorch/train_res.py
+++ pytorch/train_res.py
@@ -26,7 +26,7 @@ args.add_argument('--testing_data',
                   help='Testing data.',
                   type=str,
                   default='data/testing_data/test_list.txt')
-args.add_argument('--batch_size', help='Batch size.', type=int, default=8)
+args.add_argument('--batch_size', help='Batch size.', type=int, default=16)
 args.add_argument('--learning_rate',
                   help='Learning rate.',
                   type=float,
