786ebaa98feb102cc35c9a2d769aa84d8ba0b5f4
ge98lor <alexandre.cafaro@tum.de>
2020-04-03 18:13:12 +0200

    Update download.py

--- download.py
+++ download.py
@@ -2,10 +2,10 @@ import gdown
 import subprocess
 
 urls = [
-    'https://drive.google.com/open?id=1dKM_6sR7SCdDQXX8dIQwtpaW3qRiA7pM',
-    'https://drive.google.com/open?id=1hXq-kxEsDA0_DzcOFS0yzIvlKxwcgoGZ',
-    'https://drive.google.com/uc?id=1R69tifYSd3SRe7357ppljmU7l_WX7Wxt',
-    'https://drive.google.com/uc?id=1BzxmQlDDiXOClxkF6I2rskK8-dl2nJdV'
+    # 'https://drive.google.com/open?id=1dKM_6sR7SCdDQXX8dIQwtpaW3qRiA7pM',
+    # 'https://drive.google.com/open?id=1hXq-kxEsDA0_DzcOFS0yzIvlKxwcgoGZ',
+    # 'https://drive.google.com/uc?id=1R69tifYSd3SRe7357ppljmU7l_WX7Wxt',
+    # 'https://drive.google.com/uc?id=1BzxmQlDDiXOClxkF6I2rskK8-dl2nJdV'
     'https://drive.google.com/open?id=1tA6Vc30CdTq7QnKqxUzejY1w8gy-uFDm'
 ]
 
@@ -19,11 +19,12 @@ output = 'Test'
 print('Donwloading {} ...'.format(output))
 gdown.download(urls[-1], output, quiet=False)
 
-commands = ['mkdir pytorch/data/training_data/ShapeNetP2M/',
-            'mv training_data.001 pytorch/data/training_data/ShapeNetP2M/',
-            'mv training_data.002 pytorch/data/training_data/ShapeNetP2M/',
-            'mv training_data.003 pytorch/data/training_data/ShapeNetP2M/',
-            'mv training_data.004 pytorch/data/training_data/ShapeNetP2M/',
-            'mv Test pytorch/data/training_data/']
+commands = [
+    # 'mkdir pytorch/data/training_data/ShapeNetP2M/',
+    # 'mv training_data.001 pytorch/data/training_data/ShapeNetP2M/',
+    # 'mv training_data.002 pytorch/data/training_data/ShapeNetP2M/',
+    # 'mv training_data.003 pytorch/data/training_data/ShapeNetP2M/',
+    # 'mv training_data.004 pytorch/data/training_data/ShapeNetP2M/',
+    'mv Test pytorch/data/training_data/']
 for command in commands:
     subprocess.check_call(command.split())
