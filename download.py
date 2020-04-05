a31742d09f5f50cea4e6064eb004c2b34ef610bc
sebloun <s.lohr@tum.de>
2020-04-05 17:50:16 +0200

    fixed download.py

--- download.py
+++ download.py
@@ -2,10 +2,10 @@ import gdown
 import subprocess
 
 urls = [
-     'https://drive.google.com/uc?id=1dKM_6sR7SCdDQXX8dIQwtpaW3qRiA7pM',
-     'https://drive.google.com/uc?id=1hXq-kxEsDA0_DzcOFS0yzIvlKxwcgoGZ',
-     'https://drive.google.com/uc?id=1R69tifYSd3SRe7357ppljmU7l_WX7Wxt',
-     'https://drive.google.com/uc?id=1BzxmQlDDiXOClxkF6I2rskK8-dl2nJdV'
+    'https://drive.google.com/uc?id=1dKM_6sR7SCdDQXX8dIQwtpaW3qRiA7pM',
+    'https://drive.google.com/uc?id=1hXq-kxEsDA0_DzcOFS0yzIvlKxwcgoGZ',
+    'https://drive.google.com/uc?id=1R69tifYSd3SRe7357ppljmU7l_WX7Wxt',
+    'https://drive.google.com/uc?id=1BzxmQlDDiXOClxkF6I2rskK8-dl2nJdV',
     'https://drive.google.com/uc?id=1tA6Vc30CdTq7QnKqxUzejY1w8gy-uFDm'
 ]
 
